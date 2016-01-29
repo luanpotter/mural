cf56bce1602ba46cf6c54bfaf4a1ac2e55bbfe10
feroult <feroult@gmail.com>
2016-01-29 20:12:02 -0200

    backend

---
+++ backend/src/test/java/io/mural/utils/EndpointTestCase.java
@@ -0,0 +1,12 @@
+package io.mural.utils;
+
+import io.yawp.testing.EndpointTestCaseBase;
+
+public class EndpointTestCase extends EndpointTestCaseBase {
+
+    @Override
+    protected String getAppPackage() {
+        return "io.mural";
+    }
+
+}
