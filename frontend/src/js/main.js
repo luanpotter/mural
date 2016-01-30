3d57e79088ea985987e5a058bbd8a0dea35d790e
Luan Nico <luanpotter27@gmail.com>
2016-01-30 02:17:33 -0200

    Requiring proper interact

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,42 +1,45 @@
-
 var jQuery = require('jquery');
 var doT = require('dot');
 var yawp = require('yawp-cli');
-window.interact = require('interact');
+window.interact = require('interact.js');
 
-jQuery(function($) {
-    
-    var postTemplateFnc = doT.template($('#post-template').html());
-    var textTemplateFnc = doT.template($('#post-template-text').html());
-    var videoTemplateFnc = doT.template($('#post-template-video').html());
-    var pictureTemplateFnc = doT.template($('#post-template-picture').html());
-    
-    var handlers = {
-        'TEXTO' : textTemplateFnc,
-        'VIDEO' : videoTemplateFnc,
-        'FOTO': pictureTemplateFnc
-    };
-    
-    function createCard(post) {
-        var contentFnc = handlers[post.tipo];
-        var content = contentFnc({content: post.conteudo});
-        
-        var card = $(postTemplateFnc({title: post.titulo}));
-        $(card).find('.content').append($(content));
-        
-        return card;
-    }
-    
-    
-    function load() {
-        yawp('/posts').where('muralId', '=', '/murais/mural-da-carol').list(function (posts) {
-            posts.forEach(function(post) {
-                var card = createCard(post);
-                $('#mural').append(card);
-             });
-        });
-    }
-    
-    
-    load();
+jQuery(function ($) {
+
+	var postTemplateFnc = doT.template($('#post-template').html());
+	var textTemplateFnc = doT.template($('#post-template-text').html());
+	var videoTemplateFnc = doT.template($('#post-template-video').html());
+	var pictureTemplateFnc = doT.template($('#post-template-picture').html());
+
+	var handlers = {
+		'TEXTO': textTemplateFnc,
+		'VIDEO': videoTemplateFnc,
+		'FOTO': pictureTemplateFnc
+	};
+
+	function createCard(post) {
+		var contentFnc = handlers[post.tipo];
+		var content = contentFnc({
+			content: post.conteudo
+		});
+
+		var card = $(postTemplateFnc({
+			title: post.titulo
+		}));
+		$(card).find('.content').append($(content));
+
+		return card;
+	}
+
+
+	function load() {
+		yawp('/posts').where('muralId', '=', '/murais/mural-da-carol').list(function (posts) {
+			posts.forEach(function (post) {
+				var card = createCard(post);
+				$('#mural').append(card);
+			});
+		});
+	}
+
+
+	load();
 });
