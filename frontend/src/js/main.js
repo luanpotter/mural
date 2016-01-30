507f235a08abc655942a9cc3da3e5d4142fdd8ed
Raoni Normanton <raonifn@gmail.com>
2016-01-30 00:33:49 -0200

    fix

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,7 +1,47 @@
-// var jQuery = require('jquery');
-// var doT = require('dot');
+var jQuery = require('jquery');
+var doT = require('dot');
 var yawp = require('yawp-cli');
 
-yawp('/posts').where('muralId', '=', '/murais/mural-da-carol').list(function (posts) {
-	console.log(posts);
+jQuery(function($) {
+    
+    
+   
+    function handleText() {
+        
+    }
+    
+    function handleVideo() {
+        
+    }
+    
+    function handlePicture() {
+        
+    }
+    
+     var handlers = {
+        'TEXTO' : handleText,
+        'VIDEO' : handleVideo,
+        'FOTO': handlePicture
+    };
+    
+    function handlePost(post) {
+        var templateStr = $('#post-template').html();
+        var templateFnc = doT.template(templateStr);
+        var card = templateFnc({"title": "Olá", "content": post.conteudo});
+        var element = $(card);
+        
+        $('#mural').append(element);
+    }
+    
+    
+    function load() {
+        yawp('/posts').where('muralId', '=', '/murais/mural-da-carol').list(function (posts) {
+            posts.forEach(function(post) {
+                handlePost(post);
+             });
+        });
+    }
+    
+    
+    load();
 });
