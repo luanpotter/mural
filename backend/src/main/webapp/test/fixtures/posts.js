f09b1ef77319e42e8cf67ee61ea470c61f3d69ed
Luan Nico <luanpotter27@gmail.com>
2016-01-29 23:06:59 -0200

    adding misisng field in fixture

--- backend/src/main/webapp/test/fixtures/posts.js
+++ backend/src/main/webapp/test/fixtures/posts.js
@@ -2,17 +2,20 @@
 
     fx.post('carol1', {
         tipo: 'TEXTO',
-        conteudo: 'Carol, tudo de bom!!!'
+        conteudo: 'Carol, tudo de bom!!!',
+        muralId: '/murais/mural-da-carol'
     });
 
     fx.post('carol2', {
         tipo: 'FOTO',
-        conteudo: 'https://catraquinha.catracalivre.com.br/wp-content/uploads/sites/10/2015/03/o_show_da_luna.jpg'
+        conteudo: 'https://catraquinha.catracalivre.com.br/wp-content/uploads/sites/10/2015/03/o_show_da_luna.jpg',
+        muralId: '/murais/mural-da-carol'
     });
 
     fx.post('carol3', {
         tipo: 'VIDEO',
-        conteudo: 'geQl2cZxR7Q'
+        conteudo: 'geQl2cZxR7Q',
+        muralId: '/murais/mural-da-carol'
     });
 
 
